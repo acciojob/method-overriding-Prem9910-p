088c11514518c5cce7d765df21ab80832afc8d2e
Prem9910-p <prem60866@gmail.com>
2024-05-16 13:37:52 +0530

    initial is done

---
+++ src/main/java/A.java
@@ -0,0 +1,5 @@
+public class A {
+     public void meth(){
+        System.out.println("Invoking method from class A");
+    }
+}
---
+++ src/main/java/B.java
@@ -0,0 +1,5 @@
+public class B extends A{
+    public void meth(){
+        System.out.println("Method is overridden in Extendend class B");
+    }
+}
