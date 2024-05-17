99b8e05fc36b5df92c64a08dc52d0b2c38638ede
Prem9910-p <prem60866@gmail.com>
2024-05-17 11:41:11 +0530

    prem done

--- src/main/java/B.java
+++
@@ -1,6 +0,0 @@
-public class B extends A{
-
-   public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        A obj=new A();
-        System.out.println(obj.meth());
-        A obj2=new B();
-        System.out.println(obj2.meth());
-    }
-}
